0f904313a8ec2e71fe6b657ea46257737a528625
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-15 16:45:03 -0600

    moved PizzaList to component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import PizzaList from '../PizzaList/PizzaList';
 
 function App() {
 
@@ -10,9 +11,9 @@ function App() {
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
   
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      {/* <img src='images/pizza_photo.png' /> */}
+      {/* <p>Pizza is great.</p> */}
+      <PizzaList />
     </div>
   );
 }
---
+++ src/components/PizzaList/PizzaList.jsx
@@ -0,0 +1,10 @@
+function PizzaList() {
+    return(
+        <>
+            <h3>Step 1: Select Your Pizza</h3>
+            <p>pizza list goes here...</p>
+        </>
+    )
+}
+
+export default PizzaList;
