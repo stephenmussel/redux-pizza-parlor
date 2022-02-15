684b91a4329d6a4a6d3ff974f9c7a9b63c18183e
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-15 17:08:57 -0600

    displayed pizzaList on DOM

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function PizzaList() {
+
+    const [pizzaList, setPizzaList] = useState([]);
 
     useEffect(() => {
         console.log('in useEffect');
@@ -16,6 +18,7 @@ function PizzaList() {
             url: '/api/pizza',
         }).then(response => {
             console.log('getting pizza list: ', response.data);
+            setPizzaList(response.data);
         }).catch(error => {
             console.log('error in fetching pizza list: ', error);
         });
@@ -23,8 +26,16 @@ function PizzaList() {
 
     return(
         <>
-            <h3>Step 1: Select Your Pizza</h3>
-            <p>pizza list goes here...</p>
+            <h2>Step 1: Select Your Pizza</h2>
+            {/* {JSON.stringify(pizzaList)} */}
+            {pizzaList.map(pizza => 
+                <div>
+                    <h4>{pizza.name}</h4>
+                    <p>{pizza.description}</p>
+                    <p>{pizza.price}</p>
+                    <button>Add</button>
+                </div>
+            )}
         </>
     )
 }
