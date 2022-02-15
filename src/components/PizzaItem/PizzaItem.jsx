5cffd003049999371e86da11b38785b7d2d1a9b4
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-15 17:44:07 -0600

    captured selected pizza and ready to dispatch but need to set up redux store

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 function PizzaItem({pizza}) {
+
+    const [pizzaSelected, setPizzaSelected] = useState(false);
+    const dispatch = useDispatch();
+
+    const selectPizza = () => {
+        console.log('pizza selected: ', pizza.name); // logs pizza selected
+        setPizzaSelected(true); // sets state of pizza selected
+
+        const action = { type: 'ADD_TO_CART', payload: pizza.name }
+        dispatch(action);
+    }
+
     return(
         <>
             <h4>{pizza.name}</h4>
             <p>{pizza.description}</p>
             <p>{pizza.price}</p>
-            <button>Add</button>
+            <button onClick={selectPizza}>Add</button>
         </>
     )
 }
