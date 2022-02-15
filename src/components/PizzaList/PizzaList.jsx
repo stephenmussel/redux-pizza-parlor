314539050af033a71c2c0fd187733a98ea92842b
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-15 16:53:25 -0600

    GET pizza list from DB

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,4 +1,26 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 function PizzaList() {
+
+    useEffect(() => {
+        console.log('in useEffect');
+        fetchPizza();
+    }, [])
+
+    const fetchPizza = () => {
+        console.log('in fetchPizza');
+
+        axios({
+            method: 'GET',
+            url: '/api/pizza',
+        }).then(response => {
+            console.log('getting pizza list: ', response.data);
+        }).catch(error => {
+            console.log('error in fetching pizza list: ', error);
+        });
+    }
+
     return(
         <>
             <h3>Step 1: Select Your Pizza</h3>
