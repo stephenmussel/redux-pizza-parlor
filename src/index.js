0037af7441a30523ed964b159004d005c68ebde1
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-15 17:51:22 -0600

    created reduxStore

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaList = (state = [], action) => {
+    console.log('in pizzaList reducer');
+    return state;
+}
+
+const shoppingCart = (state = [], action) => {
+    console.log('in shoppingCart reducer');
+    return state;
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        pizzaList,
+        shoppingCart,
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
