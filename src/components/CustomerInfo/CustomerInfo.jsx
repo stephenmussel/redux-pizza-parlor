7cc18ea884171f7c348bf12697df8e0d95f4cda5
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-16 13:18:17 -0600

    captured inputs and dispatched to redux

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -1,11 +1,29 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function CustomerInfo() {
 
-    const history = useHistory();
+    const defaultCustomer = {
+        name: '',
+        address: '',
+        city: '',
+        zip: '',
+        // orderType: false
+    }
 
-    const checkout = () => {
+    const [newCustomer, setNewCustomer] = useState(defaultCustomer);
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const checkout = (event) => {
+        event.preventDefault();
         console.log('heading to checkout');
+        console.log('this is newCustomer: ', newCustomer);
+
+        const action = { type: 'ADD_CUSTOMER', payload: newCustomer}
+        dispatch(action);
         history.push ('/checkout');
     }
     return(
@@ -14,21 +32,29 @@ function CustomerInfo() {
             <form>
                 <input 
                     required 
+                    value={newCustomer.name}
+                    onChange={(event) => setNewCustomer({...newCustomer, name: event.target.value})}
                     type="text" 
                     placeholder="name"
                 /><br /><br />
                 <input 
                     required 
+                    value={newCustomer.address}
+                    onChange={(event) => setNewCustomer({...newCustomer, address: event.target.value})}
                     type="text" 
                     placeholder="street address"
                 /><br /><br />
                 <input 
                     required 
+                    value={newCustomer.city}
+                    onChange={(event) => setNewCustomer({...newCustomer, city: event.target.value})}
                     type="text" 
                     placeholder="city"
                 /><br /><br />
                 <input 
                     required 
+                    value={newCustomer.zip}
+                    onChange={(event) => setNewCustomer({...newCustomer, zip: event.target.value})}
                     type="number" 
                     placeholder="zip"
                 /><br /><br />
