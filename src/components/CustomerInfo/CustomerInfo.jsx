76f6e5a8805a909d247eb50c4813eafe7f583c51
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-16 12:59:08 -0600

    added input fields for customerInfo

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -2,6 +2,44 @@ function CustomerInfo() {
     return(
         <>
             <h2>Step 2: Customer Information</h2>
+            <form>
+                <input 
+                    required 
+                    type="text" 
+                    placeholder="name"
+                /><br /><br />
+                <input 
+                    required 
+                    type="text" 
+                    placeholder="street address"
+                /><br /><br />
+                <input 
+                    required 
+                    type="text" 
+                    placeholder="city"
+                /><br /><br />
+                <input 
+                    required 
+                    type="number" 
+                    placeholder="zip"
+                /><br /><br />
+
+                Is this order for <b>Pickup</b> or <b>Delivery</b>?<br /><br />
+
+                <label>
+                    <input 
+                        type="radio"
+                    />
+                    Pickup
+                </label>
+                <label>
+                    <input 
+                        type="radio"
+                    />
+                    Delivery
+                </label>
+                <br /><br />
+            </form>
         </>
     )
 }
