0edf93aa20ba045b09de9241c775179ca4a588dd
stephenmussel <85131376+stephenmussel@users.noreply.github.com>
2022-02-16 13:56:44 -0600

    displayed data on DOM

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,19 @@
+import { useSelector } from 'react-redux'
+
 function Checkout() {
+
+    const shoppingCart = useSelector(store => store.shoppingCart)
+    const customerInfo = useSelector(store => store.customerInfo)
+
+
     return(
         <>
             <h2>Step 3: Checkout</h2>
+            <h4>this is customer info:</h4>
+            {JSON.stringify(customerInfo)}
+            <h4>this is shopping cart:</h4>
+            {JSON.stringify(shoppingCart)}
+
         </>
     )
 }
